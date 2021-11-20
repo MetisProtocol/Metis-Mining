05d90a3e050ac63f543dff062a08d780e499e6d2
0x4c5 <secpyer@gmail.com>
2021-11-20 20:15:27 +0800

    fix bug remove state check of method `_userExist`

--- contracts/DAC.sol
+++ contracts/DAC.sol
@@ -1219,10 +1219,6 @@ contract DAC is IDAC, AccessControlUpgradeable, OwnableUpgradeable {
         if (userToDAC[user] == 0) {
             return false;
         }
-        // user was forceleft the DAC
-        if (pool[userToDAC[user]].state == DACState.Dismissed){
-            return false;
-        }
         return true;
     }
 
